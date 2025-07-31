c9354a04a55393acef09740ae48b9fb0fe889ec4
Saveliy Ivanets <ivanets300@mail.ru>
2025-07-31 05:29:02 +0300

    first

--- newStringify.js
+++ newStringify.js
@@ -1,4 +1,37 @@
+//P.S кажется что массивы и объекты работают хорошо
+// если в объекте массив то все хорошо (вроде), но если в массиве объект то начинаются страшные вещи....(я бы сказал оно чуть чуть работает...)
+// space = 0 не работает ИЗ-ЗА незначительного бага с отступами, который я могу пофиксить но после борбы с массивами у меня нет сил.......
 function myStringify(value, space) {
+  if (typeof value === "bigint") {
+    throw new TypeError("BigInt value can't be serialized in JSON");
+  }
+  if (value === undefined) {
+    return undefined;
+  }
+  if (typeof value === "function") {
+    return "null";
+  }
+  if (
+    ![
+      "Array",
+      "Object",
+      "String",
+      "Number",
+      "Boolean",
+      "Function",
+      "Null",
+      "Undefined",
+      "Date",
+    ].includes(Object.prototype.toString.call(value).slice(8, -1))
+  ) {
+    return "{}";
+  }
+  if (!isFinite(value)) {
+    return "null";
+  }
+  if (Object.prototype.toString.call(value).slice(8, -1) === "Data") {
+    return value.toISOString();
+  }
   if (!!value.toJSON && typeof value.toJSON === "function") {
     try {
       value = value.toJSON();
@@ -13,111 +46,31 @@ function myStringify(value, space) {
     return typeof value === "string" ? `"${value}"` : `${value}`;
   }
   if (Array.isArray(value)) {
-    return arrayToJSON(value, space);
+    if (space === 0) {
+      return addComma(arrayToJSON(value, space))
+        .replaceAll(",,", ",")
+        .split("\n")
+        .join("");
+    }
+    return addComma(arrayToJSON(value, space)).replaceAll(",,", ",");
   }
   if (typeof value === "object") {
+    if (space === 0) {
+      objToString(value, space, false, 2, space).split("\n").join("");
+    }
     return objToString(value, space, false, 2, space);
   }
 }
-function arrayToJSON(array, space) {
-  return doSpaceInArray(
-    stringToArray(
-      `[${array.map((item) =>
-        Array.isArray(item)
-          ? arrayToString(item)
-          : item === undefined || item === null
-          ? "null"
-          : typeof item === "string"
-          ? `"${item}"`
-          : item
-      )}]`
-    ),
-    space
-  );
+function doSpaceInObject(json, space) {
+  return json
+    .split("\n")
+    .map((item) =>
+      item.toString().includes("]") || item.toString().includes("[")
+        ? " ".repeat(space) + item
+        : " ".repeat(space) + item
+    )
+    .join("\n");
 }
-function arrayToString(arr) {
-  let newArr = arr.concat();
-  for (let i = 0; i < newArr.length; i++) {
-    if (Array.isArray(newArr[i])) {
-      newArr[i] = arrayToString(newArr[i]);
-      continue;
-    }
-    newArr[i] =
-      typeof newArr[i] === "string"
-        ? `"${newArr[i]}"`
-        : newArr[i] === null || newArr[i] === undefined
-        ? "null"
-        : // : Object.prototype.toString.call(newArr[i]).slice(8, -1) === "Object"
-          // ? objToString(newArr[i])
-          newArr[i];
-  }
-  return `[${newArr}]`;
-}
-
-function doSpaceInArray(arr, space) {
-  if (space === 0) {
-    return arr.reduce((sum, curr) => sum + curr, "");
-  }
-  let bracketCount = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== 0 && arr[i - 1].includes("[") && !arr[i - 1].includes("]")) {
-      bracketCount++;
-    } else {
-      if (i !== 0 && !arr[i].includes("[") && arr[i].includes("]")) {
-        bracketCount--;
-      }
-    }
-
-    arr[i] = `${" ".repeat(bracketCount >= 0 ? bracketCount * space : 0)}${
-      arr[i]
-    }\n`;
-  }
-  let result = arr.reduce((sum, curr) => sum + curr, "");
-  return result.slice(0, result.length - 1);
-}
-
-function stringToArray(str) {
-  let ansArr = [];
-  let k = 0;
-  let generalElem = "";
-  for (let i = 0; i < str.length; i++) {
-    if (["[", "]", ","].includes(str[i])) {
-      if (str[i] === "[" && str[i + 1] === "]") {
-        ansArr[k] = "[]";
-        k++;
-        continue;
-      }
-
-      if (str[i - 1] === "[" && str[i] === "]") {
-        continue;
-      }
-
-      if (generalElem !== "") {
-        ansArr[k] = generalElem;
-        generalElem = "";
-        k++;
-      }
-
-      if (str[i] !== ",") {
-        ansArr[k] = str[i];
-        k++;
-      } else {
-        if (str[i] === ",") {
-          if (i != 0 && str[i - 1] === ",") {
-            ansArr[k] = "null,";
-            k++;
-          } else {
-            ansArr[k - 1] += ",";
-          }
-        }
-      }
-    } else {
-      generalElem += str[i];
-    }
-  }
-  return ansArr;
-}
-
 function haveCycle(obj) {
   for (key in obj) {
     if (obj[key] === obj) {
@@ -127,7 +80,8 @@ function haveCycle(obj) {
   return false;
 }
 function objToString(usObj, space, heirFlag = false, heircount = 2, trueSpace) {
-  if (Object.keys(usObj).length === 0 && heirFlag) {
+  if (Object.keys(usObj).length === 0) {
+    return "{}";
   }
   if (haveCycle(usObj)) {
     throw new TypeError("TypeError: Converting circular structure to JSON");
@@ -141,7 +95,12 @@ function objToString(usObj, space, heirFlag = false, heircount = 2, trueSpace) {
     }
     if (
       Object.prototype.toString.call(obj[key]).slice(8, -1) === "Object" &&
-      obj[key] !== null
+      obj[key] !== null &&
+      Object.keys(obj[key]).length !== 0 &&
+      !Object.values(obj[key]).reduce(
+        (sum, curr) => sum && curr === undefined,
+        true
+      )
     ) {
       strArr.push(
         `\n${" ".repeat(space)}"${key}": ${objToString(
@@ -153,6 +112,19 @@ function objToString(usObj, space, heirFlag = false, heircount = 2, trueSpace) {
         )},`
       );
       continue;
+    } else {
+      if (
+        Object.prototype.toString.call(obj[key]).slice(8, -1) === "Object" &&
+        obj[key] !== null &&
+        (Object.keys(obj[key]).length === 0 ||
+          Object.values(obj[key]).reduce(
+            (sum, curr) => sum && curr === undefined,
+            true
+          ))
+      ) {
+        strArr.push(`\n${" ".repeat(space)}"${key}": {},`);
+        continue;
+      }
     }
     if (
       ![
@@ -178,7 +150,9 @@ function objToString(usObj, space, heirFlag = false, heircount = 2, trueSpace) {
       }
       if (Object.prototype.toString.call(obj[key]).slice(8, -1) === "Array") {
         strArr.push(
-          `\n${" ".repeat(space)}"${key}": ${arrayToJSON(obj[key], space)
+          `\n${" ".repeat(space)}"${key}": ${addComma(
+            arrayToJSON(obj[key], space)
+          )
             .split("\n")
             .map((item) => {
               if (item !== "[" && item !== "]") {
@@ -194,6 +168,14 @@ function objToString(usObj, space, heirFlag = false, heircount = 2, trueSpace) {
             })
             .join("\n")},`
         );
+        continue;
+      }
+      if (Object.prototype.toString.call(obj[key]).slice(8, -1) === "Number") {
+        if (isFinite(obj[key])) {
+          strArr.push(`\n${" ".repeat(space)}"${key}": ${obj[key]},`);
+        } else {
+          strArr.push(`\n${" ".repeat(space)}"${key}": null,`);
+        }
         continue;
       }
     }
@@ -219,17 +201,254 @@ function objToString(usObj, space, heirFlag = false, heircount = 2, trueSpace) {
   );
   return strArr.reduce((sum, curr) => sum + curr, "");
 }
+function doArrayInCorrectForm(arr) {
+  let ansArr = arr.concat();
+  if (ansArr.length > 0 && ansArr.filter(() => true).length === 0) {
+    let newAnsArr = [];
+    for (let i = 0; i < ansArr.length; i++) {
+      newAnsArr.push("null");
+    }
+    return newAnsArr;
+  } else {
+    if (ansArr.length > 0 && ansArr.filter(() => true).length > 0) {
+      for (let i = 0; i < ansArr.length; i++) {
+        ansArr[i] = i in ansArr ? ansArr[i] : null;
+      }
+    }
+  }
 
-let space = 2;
-const check = { a: {} };
+  return ansArr.map((item) =>
+    item === null ||
+    item === undefined ||
+    typeof item === "function" ||
+    (typeof item === "number" && !isFinite(item))
+      ? "null"
+      : typeof item === "string" && !item.includes("[") && !item.includes("]")
+      ? `"${item}"`
+      : Array.isArray(item)
+      ? doArrayInCorrectForm(item)
+      : Object.prototype.toString.call(item).slice(8, -1) === "Date"
+      ? `"${item.toISOString()}"`
+      : ![
+          "Array",
+          "Object",
+          "String",
+          "Number",
+          "Boolean",
+          "Function",
+          "Null",
+          "Undefined",
+          "Date",
+        ].includes(Object.prototype.toString.call(item).slice(8, -1))
+      ? "{}"
+      : item
+  );
+}
+function arrayToJSON(array, space, enclosure = 1) {
+  if (space > 10) {
+    space = 10;
+  }
+  let jsonArray = array.concat();
+  let bracketCount = 0;
 
-let my = myStringify(check, space);
-let their = JSON.stringify(check, null, space);
+  jsonArray.unshift(`${" ".repeat((enclosure - 1) * space)}[\n`);
+  jsonArray.push(`${" ".repeat((enclosure - 1) * space)}]`);
+  if (enclosure === 1) {
+    jsonArray = doArrayInCorrectForm(jsonArray);
+  }
+  for (let i = 0; i < jsonArray.length; i++) {
+    if (Array.isArray(jsonArray[i])) {
+      if (jsonArray[i].length === 0) {
+        jsonArray[i] =
+          i + 1 !== jsonArray.length &&
+          jsonArray[i + 1].toString().includes("]")
+            ? `${" ".repeat(enclosure * space)}[]\n`
+            : `${" ".repeat(enclosure * space)}[],\n`;
+      } else {
+        jsonArray[i] = `${arrayToJSON(jsonArray[i], space, enclosure + 1)}\n`;
+        bracketCount =
+          !Array.isArray(jsonArray[i + 1]) && i + 1 != jsonArray.length
+            ? space * enclosure
+            : bracketCount;
+        continue;
+      }
+    }
+    if (
+      Object.prototype.toString.call(jsonArray[i]).slice(8, -1) === "Object"
+    ) {
+      jsonArray[i] =
+        i + 1 !== jsonArray.length && jsonArray[i + 1].toString().includes("]")
+          ? `${doSpaceInObject(
+              objToString(jsonArray[i], space, false, 2, space),
+              space * enclosure
+            )}\n`
+          : `${doSpaceInObject(
+              objToString(jsonArray[i], space, false, 2, space),
+              space * enclosure
+            )}\n`;
+      bracketCount += space * enclosure;
+      continue;
+    }
+    if (
+      i !== 0 &&
+      jsonArray[i - 1].toString().includes("[") &&
+      !jsonArray[i - 1].toString().includes("]")
+    ) {
+      bracketCount += space * enclosure;
+    } else {
+      if (
+        !jsonArray[i].toString().includes("[") &&
+        jsonArray[i].toString().includes("]")
+      ) {
+        bracketCount -= space * (enclosure - 1);
+      }
+    }
+    bracketCount = bracketCount < 0 ? 0 : bracketCount;
+    if (
+      !jsonArray[i].toString().includes("]") &&
+      !jsonArray[i].toString().includes("[")
+    ) {
+      jsonArray[i] =
+        i + 1 !== jsonArray.length && jsonArray[i + 1].toString().includes("]")
+          ? `${" ".repeat(bracketCount)}${jsonArray[i]}\n`
+          : `${" ".repeat(bracketCount)}${jsonArray[i]},\n`;
+    }
+  }
+
+  return jsonArray.reduce((sum, curr) => sum + curr, "");
+}
+function addComma(json) {
+  json = json.split("\n");
+  for (let i = 0; i < json.length - 1; i++) {
+    if (
+      (json[i].toString().includes("]") || json[i].toString().includes("}")) &&
+      (!json[i + 1].toString().includes("]") ||
+        json[i + 1].toString().includes("[]")) &&
+      !json[i].toString().includes("[]") &&
+      (!json[i + 1].toString().includes("}") ||
+        json[i + 1].toString().includes("{}"))
+    ) {
+      json[i] += ",";
+    }
+  }
+  return json.join("\n");
+}
+let space = 7;
+const testArray = [
+  1,
+  true,
+  "str",
+  null,
+  undefined,
+  NaN,
+  Infinity,
+  [[[[[[[]]]]]]],
+  new Set(),
+  new Map(),
+  new Date(),
+  function () {},
+  [, , , , , , ,],
+  new Array(2),
+  {},
+  { 1: 1, 2: 2, 3: 3 },
+  [
+    ,
+    ,
+    ,
+    "NUMBERS AND NULLS:",
+    ,
+    ,
+    1,
+    ,
+    ,
+    1,
+    ,
+    ,
+    ,
+    new Array(2),
+    ,
+    ,
+    [1, 1],
+    [
+      1,
+      1,
+      1,
+      [1, , 1, 1, 1, 1, [1, 1]],
+      1,
+      2,
+      2,
+      [1, 1, 1, [1, 1, 1, 1, [1, [1]]]],
+    ],
+  ],
+  [[[[]][[]]]], // - мой любимый кейс
+  [],
+];
+const testObject = {
+  num: 12,
+  bool: true,
+  str: "string",
+  nul: null,
+  und: undefined,
+  child: { grandson: { name: "Jhon" }, job: {} },
+  func: function (a) {},
+  map: new Map(),
+  set: new Set(),
+  data: new Date(),
+  Nan: NaN,
+  Infinity: Infinity,
+  wow: {},
+  easyArr: [1, 2, 3, 4],
+  hardArr: [
+    1,
+    2,
+    3,
+    true,
+    null,
+    undefined,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    function () {},
+    [[[[[[[["wow"]]]]]]]],
+    { objInArray: true },
+    [
+      ,
+      ,
+      ,
+      "NUMBERS AND NULLS:",
+      ,
+      ,
+      1,
+      ,
+      ,
+      1,
+      ,
+      ,
+      ,
+      new Array(2),
+      ,
+      ,
+      [1, 1],
+      [
+        1,
+        1,
+        1,
+        [1, , 1, 1, 1, 1, [1, 1]],
+        1,
+        2,
+        2,
+        [1, 1, 1, [1, 1, 1, 1, [1, [1]]]],
+      ],
+    ],
+  ],
+  empty: [],
+};
+let my = myStringify(testObject, space);
+let their = JSON.stringify(testObject, null, space);
 console.log(my === their);
 console.log(my);
 console.log();
 console.log(their);
-// не работает с пустыми объектами и массивами
-// не работает если в массиве объект
-// arraytoString -> stringToArray - > doSpaceInArray -> arrayToJSON
-// arrayToString -
